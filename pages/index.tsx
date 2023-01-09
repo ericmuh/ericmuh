f345c347a2775dd3ab2a0020790a6d372c681a0c
ericmuh <muhwezi1000@gmail.com>
2023-01-09 22:28:47 +0300

    update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,6 @@ function Home() {
       <main>
         <h1 className="text-xl"> I am Eric Muhwezi</h1>
       </main>
-      <footer className={styles.footer}>Powered by NextJS</footer>
     </div>
   );
 }

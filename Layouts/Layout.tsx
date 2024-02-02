81c3bce552717e2b477fafa541abc5242a03b891
Eric Muhwezi <muhwezi1000@gmail.com>
2024-02-02 17:47:55 +0300

    update to nextjs 14

---
+++ Layouts/Layout.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+function Layout() {
+  return (
+    <Head>
+      <title>Eric Muhwezi</title>
+      <meta name="description" content="A personal website by Eric Muhwezi" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
+
+export default Layout;
